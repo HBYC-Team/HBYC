56febe9d528f0f4ddeecffce9dd19126f897152c
dragonyc1002 <mivck1012@gmail.com>
2022-10-10 13:26:19 +0800

    chore: add missing content

--- src/cmds/announcement.js
+++ src/cmds/announcement.js
@@ -22,7 +22,7 @@ module.exports = {
       .setColor(0x1cb7ca)
       .setTitle(`最新版本公告 - ${bot.version}`)
       .addFields(
-        { name: ">> 使用者方面", value: "+ </say:> 指令再也不能夠 @everyone 或 @here 了\n+ 重新整理並修復整份機器人存在的 bug\n+ 若是遊戲指令的參數錯誤，機器人會回應你一些話\n+ 修改使用者條款中的一些錯誤字詞" },
+        { name: ">> 使用者方面", value: "+ </say:992404868927078481> 指令再也不能夠 @everyone 或 @here 了\n+ 重新整理並修復整份機器人存在的 bug\n+ 若是遊戲指令的參數錯誤，機器人會回應你一些話\n+ 修改使用者條款中的一些錯誤字詞" },
         { name: ">> 專案庫方面", value: "+ 移動整份專案至新的 repository\n+ Clean up 整份專案，進行大量 refactor\n+ 新的 banner\n+ 將 License 的部份重新闡述\n+ 改用 yarn 作為套件管理" }
       )
       .setTimestamp()
