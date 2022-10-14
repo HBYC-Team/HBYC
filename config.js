568c4c124e39be5f0f158481165850e287862068
dragonyc1002 <mivck1012@gmail.com>
2022-10-14 20:51:42 +0800

    feature: add osu!api key config property

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 
-if(!process.env.TOKEN) throw new Error('Bot token has not been configured');
-if(!process.env.clientId) throw new Error('Bot client id has not been configured');
-if(!process.env.cmdHookId) throw new Error('Command Log Webhook id has not been configured');
-if(!process.env.cmdHookToken) throw new Error('Command Log Webhook token has not been configured');
-if(!process.env.msgHookId) throw new Error('Message Log Webhook id has not been configured');
-if(!process.env.msgHookToken) throw new Error('Message Log Webhook token has not been configured');
-if(!process.env.botHookId) throw new Error('Bot Status Log Webhook id has not been configured');
-if(!process.env.botHookToken) throw new Error('Bot Status Log Webhook token has not been configured');
-if(!process.env.errHookId) throw new Error('Error Log Webhook id has not been configured');
-if(!process.env.errHookToken) throw new Error('Error Log Webhook token has not been configured');
-if(!process.env.reportHookId) throw new Error('Report Log Webhook id has not been configured');
-if(!process.env.reportHookToken) throw new Error('Report Log Webhook token has not been configured');
+if(!process.env.TOKEN) throw new Error('Bot token has not been configured.');
+if(!process.env.clientId) throw new Error('Bot client id has not been configured.');
+if(!process.env.cmdHookId) throw new Error('Command Log Webhook id has not been configured.');
+if(!process.env.cmdHookToken) throw new Error('Command Log Webhook token has not been configured.');
+if(!process.env.msgHookId) throw new Error('Message Log Webhook id has not been configured.');
+if(!process.env.msgHookToken) throw new Error('Message Log Webhook token has not been configured.');
+if(!process.env.botHookId) throw new Error('Bot Status Log Webhook id has not been configured.');
+if(!process.env.botHookToken) throw new Error('Bot Status Log Webhook token has not been configured.');
+if(!process.env.errHookId) throw new Error('Error Log Webhook id has not been configured.');
+if(!process.env.errHookToken) throw new Error('Error Log Webhook token has not been configured.');
+if(!process.env.reportHookId) throw new Error('Report Log Webhook id has not been configured.');
+if(!process.env.reportHookToken) throw new Error('Report Log Webhook token has not been configured.');
+
+if(!process.env.osuApiKey) throw new Error('Osu! Api Key has not been configured.')
 
 module.exports = {
   bot: {
@@ -37,5 +39,8 @@ module.exports = {
   reportHook: {
     id: process.env.reportHookId,
     token: process.env.reportHookToken
+  },
+  osu: {
+    apiKey: process.env.osuApiKey
   }
 }
