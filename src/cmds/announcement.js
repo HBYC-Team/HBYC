5f6db7c9af05b54580046b2b5484a4286691007e
dragonyc1002 <mivck1012@gmail.com>
2022-11-06 12:36:10 +0800

    feature: import ann data from new module

--- src/cmds/announcement.js
+++ src/cmds/announcement.js
@@ -1,15 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder, WebhookClient } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const { bot } = require('../constants.json');
 
-const config = require('../../config');
-
-const {cmdHook} = require("../hooks");
+const ann = require('../data/latestUpdate');
+const { cmdHook } = require('../utils/WebhookManager');
 
 const announcementData = new SlashCommandBuilder()
   .setName("announcement")
   .setDescription("目前最新版本的公告")
-
 
 module.exports = {
   data: announcementData,
@@ -19,13 +17,14 @@ module.exports = {
       .setColor(0x1cb7ca)
       .setTitle(`最新版本公告 - ${bot.version}`)
       .addFields(
-        { name: ">> 使用者方面", value: "+ 解決 </buttonrole:1009671086952169483> 的按鈕在一段時間後就會失效的狀況\n + </buttonrole:1009671086952169483> 如果使用者或機器人權限不足將會直接回應" },
-        { name: ">> 專案庫方面", value: "+ 將 `deploy.js` 重新彙整，現在可以註冊私人測試指令了\n+ 當有人觸發交互 console 皆會 log ，以彌補機器人會常常重啟 (cycling 的關係 ) 的問題" }
+        { name: "使用者方面", value: ann.userUpdate },
+        { name: "專案庫方面", value: ann.devUpdate }
       )
       .setTimestamp()
-      .setFooter({ text: "更詳細的內容請到Github上查看喔！" });
+      .setFooter({ text: "更詳細的內容請到 Github 上查看喔！" });
 
     await interaction.reply({ embeds: [annEmbed] });
+    await interaction.channel.send(ann.supportGuildAnn)
     
     const cmdHookEmbed = new EmbedBuilder()
       .setAuthor({ name: "Command Log", iconURL: interaction.client.user.avatarURL() })
