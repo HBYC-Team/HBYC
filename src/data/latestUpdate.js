e697451eb3485c5966cbfa9084122b045467a575
dragonyc1002 <mivck1012@gmail.com>
2022-11-11 20:55:25 +0800

    feature: latestUpdate content manage

---
+++ src/data/latestUpdate.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'userUpdate': '+ 於 `/osu userinfo` 指令新增了一些內容\n+ 新增 `/lightsUp` （點燈）遊戲\n+ 將 `/avatar` 指令產生的錯誤修復',
+  'devUpdate': '+ 新增 `/sendann` 私人指令\n+ 新增 `clear` 檔案用於清除已部署的指令\n+ 將 Webhook 的 Object 全部交由 `WebhookManager` 管理\n+ 將一些程式碼進行優化',
+
+  'supportGuildAnn': `\`\`\`
+>> 使用者方面
+  + 於 \`/osu userinfo\` 指令新增了一些內容
+  + 新增 \`/lightsUp\` （點燈）遊戲
+  + 將 \`/avatar\` 指令產生的錯誤修復
+
+>> 專案庫方面
+  + 新增 \`/sendann\` 私人指令
+  + 新增 \`clear\` 檔案用於清除已部署的指令
+  + 將 Webhook 的 Object 全部交由 \`WebhookManager\` 管理
+  + 將一些程式碼進行優化
+
+>> 其他方面
+  - 將機器人託管平台由 heroku.com 改成 glitch.com\`\`\``
+}
+
